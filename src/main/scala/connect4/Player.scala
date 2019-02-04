3248cf023d25bbb2ff139dc2b09afc4de2a0de8e
Aytac Ozkan <aytac.ozkn@gmail.com>
2019-02-04 01:45:45 +0100

    dockerfile is added, Connect4.scala ComputerPlayer.scala DefaultEvaluation.scala HumanPlayer.scala
    are added.

--- src/main/scala/connect4/Player.scala
+++ src/main/scala/connect4/Player.scala
@@ -6,11 +6,12 @@ import connect4.Discs.Disc
   * Represents a Player.
   */
 trait Player {
+    
     val name:String
     val disc:Disc
 
     /**
-      * 
+      * Determines next move based on given Grid state.
       * @param grid
       * @return  Column in which to drop a disc.
       */
